419c1a7012541545dacc6db754e534f54be2a254
zcysxy <zcysxy.wyzdz@gmail.com>
2025-05-18 04:46:56 -0400

    change footer

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -6,7 +6,7 @@ export const sharedPageComponents: SharedLayout = {
   head: Component.Head(),
   header: [],
   afterBody: [],
-  footer: Component.Footer()
+  footer: Component.MyFooter()
 }
 
 // components for pages that display a single page (e.g. a single note)
