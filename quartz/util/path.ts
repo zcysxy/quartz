ac51470303e4ff720b75fd5f717607f7318bb348
zcysxy <zcysxy.wyzdz@gmail.com>
2025-06-04 18:42:15 -0400

    mod sluggify to lowercase

--- quartz/util/path.ts
+++ quartz/util/path.ts
@@ -56,6 +56,7 @@ export function getFullSlug(window: Window): FullSlug {
 
 function sluggify(s: string): string {
   return s
+		.toLowerCase()
     .split("/")
     .map((segment) =>
       segment
